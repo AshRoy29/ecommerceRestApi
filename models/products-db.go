06a7c17ec893332ab6c5be1b8a906fef75a5a5e2
AshRoy29 <ash.rox2920@gmail.com>
2021-11-20 23:56:26 +0600

    Initial commit

---
+++ models/products-db.go
@@ -0,0 +1,136 @@
+package models
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+type DBModel struct {
+	DB *sql.DB
+}
+
+// Get returns one movie and error, if any
+func (m *DBModel) Get(id int) (*Product, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, title, price, description, image,
+				created_at, updated_at from products where id = $1
+	`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var product Product
+
+	err := row.Scan(
+		&product.ID,
+		&product.Title,
+		&product.Price,
+		&product.Description,
+		&product.Image,
+		&product.CreatedAt,
+		&product.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	query = `select
+				pc.id, pc.product_id, pc.category_id, c.category_name
+			from
+				products_category pc
+				left join category c on (c.id = pc.category_id)
+			where
+				pc.product_id = $1
+	`
+
+	rows, _ := m.DB.QueryContext(ctx, query, id)
+	defer rows.Close()
+
+	category := make(map[int]string)
+	for rows.Next() {
+		var pc ProductCategory
+		err := rows.Scan(
+			&pc.ID,
+			&pc.ProductID,
+			&pc.CategoryID,
+			&pc.Category.CategoryName,
+		)
+		if err != nil {
+			return nil, err
+		}
+		category[pc.ID] = pc.Category.CategoryName
+	}
+
+	product.ProductCategory = category
+
+	return &product, nil
+}
+
+// All returns all products and error, if any
+func (m *DBModel) All() ([]*Product, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, title, price, description, image,
+				created_at, updated_at from products order by title
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*Product
+
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(
+			&product.ID,
+			&product.Title,
+			&product.Price,
+			&product.Description,
+			&product.Image,
+			&product.CreatedAt,
+			&product.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		// get categories
+		categoryQuery := `select
+			pc.id, pc.product_id, pc.category_id, c.category_name
+		from
+			products_category pc
+			left join category c on (c.id = pc.category_id)
+		where
+			pc.product_id = $1
+		`
+
+		categoryRows, _ := m.DB.QueryContext(ctx, categoryQuery, product.ID)
+
+		categories := make(map[int]string)
+		for categoryRows.Next() {
+			var pc ProductCategory
+			err := categoryRows.Scan(
+				&pc.ID,
+				&pc.ProductID,
+				&pc.CategoryID,
+				&pc.Category.CategoryName,
+			)
+			if err != nil {
+				return nil, err
+			}
+			categories[pc.CategoryID] = pc.Category.CategoryName
+		}
+		categoryRows.Close()
+
+		product.ProductCategory = categories
+		products = append(products, &product)
+
+	}
+	return products, nil
+}
