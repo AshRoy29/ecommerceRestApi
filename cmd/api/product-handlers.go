87ecd33f457b7de547535d598f8f95ac59a5f697
AshRoy29 <ash.rox2920@gmail.com>
2021-11-23 00:04:05 +0600

    Initial commit

--- cmd/api/product-handlers.go
+++ cmd/api/product-handlers.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"ecom-api/models"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 )
+
+type ProductPayload struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Price       string `json:"price"`
+	Description string `json:"description"`
+	//Image string `json:"image"`
+
+}
 
 func (app *application) getOneProduct(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
@@ -46,7 +60,35 @@ func (app *application) deleteProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) insertProduct(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 * 1024 * 1024)
+	file, handler, err := r.FormFile("image")
 
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Println("file info")
+	fmt.Println("file name:", handler.Filename)
+	fmt.Println("file size:", handler.Size)
+	fmt.Println("file type:", handler.Header.Get("Content-Type"))
+
+	tempFile, err := ioutil.TempFile("img", "img-*.jpg")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	tempFile.Write(fileBytes)
+
+	fmt.Println("SUCCESS")
 }
 
 func (app *application) updateProduct(w http.ResponseWriter, r *http.Request) {
@@ -58,6 +100,29 @@ func (app *application) searchProducts(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) test(w http.ResponseWriter, r *http.Request) {
+
+	var payload ProductPayload
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println(payload.Title)
+
+	var product models.Product
+
+	product.ID, _ = strconv.Atoi(payload.ID)
+	product.Title = payload.Title
+	product.Price, _ = strconv.Atoi(payload.Price)
+	product.Description = payload.Description
+
+	//app.insertProduct(w, r)
+
+	log.Println(product.Price)
+
 	type jsonResp struct {
 		OK bool `json:"ok"`
 	}
@@ -66,9 +131,10 @@ func (app *application) test(w http.ResponseWriter, r *http.Request) {
 		OK: true,
 	}
 
-	err := app.writeJSON(w, http.StatusOK, ok, "response")
+	err = app.writeJSON(w, http.StatusOK, ok, "response")
 	if err != nil {
 		app.errorJSON(w, err)
 		return
 	}
+
 }
