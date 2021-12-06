4dd15551d59ad2caf3785fc105b6705192d27a5d
AshRoy29 <ash.rox2920@gmail.com>
2021-12-07 00:43:57 +0600

    Updated commit

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -22,17 +22,27 @@ type UserPayload struct {
 }
 
 //dummy user for development
-var validUser = models.User{
-
-	ID:          10,
-	Email:       "admin@admin.com",
-	Password:    "$2a$12$p8cKatzIyII2V.QbOY9CcOhj23.PLblcu7E.ja42TX3ghWyRRtjjC", //Password: password
-	AccessLevel: 2,
-}
+//var validUser = models.User{
+//
+//	ID:          10,
+//	Email:       "admin@admin.com",
+//	Password:    "$2a$12$p8cKatzIyII2V.QbOY9CcOhj23.PLblcu7E.ja42TX3ghWyRRtjjC", //Password: password
+//	AccessLevel: "admin",
+//}
 
 type Credentials struct {
 	Username string `json:"email"`
 	Password string `json:"password"`
+}
+
+type Token struct {
+	ID          int    `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Phone       string `json:"phone"`
+	Email       string `json:"email"`
+	AccessLevel string `json:"access_level"`
+	JwtBytes    []byte `json:"jwt"`
 }
 
 func (app *application) signup(w http.ResponseWriter, r *http.Request) {
@@ -51,7 +61,13 @@ func (app *application) signup(w http.ResponseWriter, r *http.Request) {
 	user.FirstName = payload.FirstName
 	user.LastName = payload.LastName
 	user.Phone = payload.Phone
-	user.Email = payload.Email
+	checkEmail, _ := app.models.DB.CheckEmail(payload.Email)
+	if checkEmail != "" {
+		app.errorJSON(w, errors.New("email already in use"))
+		return
+	} else {
+		user.Email = payload.Email
+	}
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(payload.Password), 12)
 	user.Password = string(hashedPassword)
 
@@ -71,7 +87,9 @@ func (app *application) signin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	hashedPassword, accessLevel, _ := app.models.DB.ValidUser(creds.Username)
+	validUser, err := app.models.DB.ValidUser(creds.Username)
+
+	hashedPassword := validUser.Password
 
 	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password))
 	if err != nil {
@@ -87,12 +105,20 @@ func (app *application) signin(w http.ResponseWriter, r *http.Request) {
 	claim.Issuer = "mydomain.com"
 	claim.Audiences = []string{"mydomain.com"}
 
-	jwtBytes, err := claim.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+	var token Token
+
+	token.ID = validUser.ID
+	token.FirstName = validUser.FirstName
+	token.LastName = validUser.LastName
+	token.Phone = validUser.Phone
+	token.Email = validUser.Email
+	token.AccessLevel = validUser.AccessLevel
+
+	token.JwtBytes, err = claim.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
 	if err != nil {
 		app.errorJSON(w, errors.New("error signing in"))
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, string(jwtBytes), "response")
-	app.writeJSON(w, http.StatusOK, accessLevel, "access_level")
+	app.writeJSON(w, http.StatusOK, token, "response")
 }
